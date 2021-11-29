741338d6fd7c7d47be4c678e6234ea3a51a7875d
aleksandra <oleksandra.klevchuk@gmail.com>
2021-11-29 10:57:58 -0600

    add solution for part 4.

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import uuid from "uuid";
 import axios from "axios";
 
-const useAxios = (url) => {
+const useAxios = (baseUrl) => {
   const [response, setResponse] = useState([]);
 
-  const getData = async () => {
+  const getData = async (restOfUrl = "") => {
+    const url =
+      typeof restOfUrl === "string" ? `${baseUrl}${restOfUrl}` : baseUrl;
     const res = await axios.get(url);
     setResponse((response) => [...response, { ...res.data, id: uuid() }]);
   };
